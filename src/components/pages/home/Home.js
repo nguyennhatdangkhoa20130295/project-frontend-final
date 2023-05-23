03432b0f7aaf5db5236b70da979a976d164d1eff
hothanhnghia-nlu <nghiant369@gmail.com>
2023-05-23 16:03:44 +0700

    Create dynamic title and set header logo

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,8 +1,12 @@
 import React from "react";
 import HotNews from "./mainContent/HotNews";
 import RecentNews from "./mainContent/RecentNews";
+import {TabTitle} from "../../../utils/DynamicTitle";
 
 const Home = () => {
+    // Dynamic Title
+    TabTitle('THỂ THAO 247');
+
     return (
         <>
             <HotNews/>
