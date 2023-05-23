d46de751c231dbe5bd2812f3f664bf9dc46f45ce
DangKhoa <20130295@st.hcmuaf.edu.vn>
2023-05-23 15:03:29 +0700

    Divide the remaining components

---
+++ src/components/pages/home/mainContent/SideContents.js
@@ -0,0 +1,207 @@
+import React from "react";
+const SideContents = () =>{
+    return(
+        <div className="col-lg-3 col-md-12 col-sm-12 col-xs-12">
+            <div className="sidebar">
+                <div className="widget">
+                    <div className="banner-spot clearfix">
+                        <div className="banner-img">
+                            <img src="/assets/upload/banner_07.jpg" alt="" className="img-fluid"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="widget">
+                    <h2 className="widget-title">Trend Videos</h2>
+                    <div className="trend-videos">
+                        <div className="blog-box">
+                            <div className="post-media">
+                                <a href="tech-single.html" title="">
+                                    <img src="/assets/upload/tech_video_01.jpg" alt="" className="img-fluid"/>
+                                    <div className="hovereffect">
+                                        <span className="videohover"></span>
+                                    </div>
+                                </a>
+                            </div>
+                            <div className="blog-meta">
+                                <h4><a href="tech-single.html" title="">We prepared the best 10 laptop
+                                    presentations for you</a></h4>
+                            </div>
+                        </div>
+
+                        <hr className="invis"/>
+
+                        <div className="blog-box">
+                            <div className="post-media">
+                                <a href="tech-single.html" title="">
+                                    <img src="/assets/upload/tech_video_02.jpg" alt="" className="img-fluid"/>
+                                    <div className="hovereffect">
+                                        <span className="videohover"></span>
+                                    </div>
+                                </a>
+                            </div>
+                            <div className="blog-meta">
+                                <h4><a href="tech-single.html" title="">We are guests of ABC Design
+                                    Studio - Vlog</a></h4>
+                            </div>
+                        </div>
+
+                        <hr className="invis"/>
+
+                        <div className="blog-box">
+                            <div className="post-media">
+                                <a href="tech-single.html" title="">
+                                    <img src="/assets/upload/tech_video_03.jpg" alt=""
+                                         className="img-fluid"/>
+                                    <div className="hovereffect">
+                                        <span className="videohover"></span>
+                                    </div>
+                                </a>
+                            </div>
+                            <div className="blog-meta">
+                                <h4><a href="tech-single.html" title="">Both blood pressure monitor
+                                    and intelligent clock</a></h4>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+
+                <div className="widget">
+                    <h2 className="widget-title">Popular Posts</h2>
+                    <div className="blog-list-widget">
+                        <div className="list-group">
+                            <a href="tech-single.html"
+                               className="list-group-item list-group-item-action flex-column align-items-start">
+                                <div className="w-100 justify-content-between">
+                                    <img src="/assets/upload/tech_blog_08.jpg" alt=""
+                                         className="img-fluid float-left"/>
+                                    <h5 className="mb-1">5 Beautiful buildings you need..</h5>
+                                    <small>12 Jan, 2016</small>
+                                </div>
+                            </a>
+
+                            <a href="tech-single.html"
+                               className="list-group-item list-group-item-action flex-column align-items-start">
+                                <div className="w-100 justify-content-between">
+                                    <img src="/assets/upload/tech_blog_01.jpg" alt=""
+                                         className="img-fluid float-left"/>
+                                    <h5 className="mb-1">Let's make an introduction for..</h5>
+                                    <small>11 Jan, 2016</small>
+                                </div>
+                            </a>
+
+                            <a href="tech-single.html"
+                               className="list-group-item list-group-item-action flex-column align-items-start">
+                                <div className="w-100 last-item justify-content-between">
+                                    <img src="/assets/upload/tech_blog_03.jpg" alt=""
+                                         className="img-fluid float-left"/>
+                                    <h5 className="mb-1">Did you see the most beautiful..</h5>
+                                    <small>07 Jan, 2016</small>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="widget">
+                    <h2 className="widget-title">Recent Reviews</h2>
+                    <div className="blog-list-widget">
+                        <div className="list-group">
+                            <a href="tech-single.html"
+                               className="list-group-item list-group-item-action flex-column align-items-start">
+                                <div className="w-100 justify-content-between">
+                                    <img src="/assets/upload/tech_blog_02.jpg" alt=""
+                                         className="img-fluid float-left"/>
+                                    <h5 className="mb-1">Banana-chip chocolate cake recipe..</h5>
+                                    <span className="rating">
+                                                    <i className="fa fa-star"></i>
+                                                    <i className="fa fa-star"></i>
+                                                    <i className="fa fa-star"></i>
+                                                    <i className="fa fa-star"></i>
+                                                    <i className="fa fa-star"></i>
+                                                </span>
+                                </div>
+                            </a>
+
+                            <a href="tech-single.html"
+                               className="list-group-item list-group-item-action flex-column align-items-start">
+                                <div className="w-100 justify-content-between">
+                                    <img src="/assets/upload/tech_blog_03.jpg" alt=""
+                                         className="img-fluid float-left"/>
+                                    <h5 className="mb-1">10 practical ways to choose organic..</h5>
+                                    <span className="rating">
+                                                    <i className="fa fa-star"></i>
+                                                    <i className="fa fa-star"></i>
+                                                    <i className="fa fa-star"></i>
+                                                    <i className="fa fa-star"></i>
+                                                    <i className="fa fa-star"></i>
+                                                </span>
+                                </div>
+                            </a>
+
+                            <a href="tech-single.html"
+                               className="list-group-item list-group-item-action flex-column align-items-start">
+                                <div className="w-100 last-item justify-content-between">
+                                    <img src="/assets/upload/tech_blog_07.jpg" alt=""
+                                         className="img-fluid float-left"/>
+                                    <h5 className="mb-1">We are making homemade ravioli..</h5>
+                                    <span className="rating">
+                                                    <i className="fa fa-star"></i>
+                                                    <i className="fa fa-star"></i>
+                                                    <i className="fa fa-star"></i>
+                                                    <i className="fa fa-star"></i>
+                                                    <i className="fa fa-star"></i>
+                                                </span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="widget">
+                    <h2 className="widget-title">Follow Us</h2>
+
+                    <div className="row text-center">
+                        <div className="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+                            <a href="home#" className="social-button facebook-button">
+                                <i className="fa fa-facebook"></i>
+                                <p>27k</p>
+                            </a>
+                        </div>
+
+                        <div className="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+                            <a href="home#" className="social-button twitter-button">
+                                <i className="fa fa-twitter"></i>
+                                <p>98k</p>
+                            </a>
+                        </div>
+
+                        <div className="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+                            <a href="home#" className="social-button google-button">
+                                <i className="fa fa-google-plus"></i>
+                                <p>17k</p>
+                            </a>
+                        </div>
+
+                        <div className="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+                            <a href="home#" className="social-button youtube-button">
+                                <i className="fa fa-youtube"></i>
+                                <p>22k</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="widget">
+                    <div className="banner-spot clearfix">
+                        <div className="banner-img">
+                            <img src="/assets/upload/banner_03.jpg" alt="" className="img-fluid"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default SideContents;
---
+++ src/components/pages/singlePage/SpecificPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PageTitle from "./mainContent/PageTitle";
+import MainContent from "./mainContent/MainContent";
+const SpecificPage = () =>{
+    return(
+      <>
+          <PageTitle/>
+          <MainContent/>
+      </>
+    );
+}
+export default SpecificPage;
